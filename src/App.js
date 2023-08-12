11f8d6eb33f29e7bd3e51628dac4539ab58adf5d
Will <willyknapp@gmail.com>
2023-08-12 00:48:10 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
+import { useState } from "react";
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [navClasses, setNavClasses] = useState(["active","",""]);
+  const [hiddenTabs, setHiddenTabs] = useState([""," hidden"," hidden"]);
+
+  function tabClick(tabName) {
+    let index = 0;
+    switch(tabName) { //If the switch is put in reverse tab order, each level of case can add one to an index int and make it easier to add future tabs because all that needs to happen is add an incrementing case at the top of the switch
+      case "search":  index++;
+      case "queue":   index++;
+      case "home":
+      default:        index+=0;
+    }
+    let navArr = new Array(navClasses.length).fill("Nav-tab");
+    let hiddenArr = new Array(hiddenTabs.length).fill(" hidden");
+    navArr[index] += " active";
+    hiddenArr[index] = "";
+    console.log(hiddenArr);
+    setNavClasses(navArr);
+    setHiddenTabs(hiddenArr);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className = "Logo">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Partyfy</h1>
+        </div>
+        <div className="Nav-bar">
+          <p className={navClasses[0]} onClick={() => tabClick("home")} name="home">Home</p>
+          <p className={navClasses[1]} onClick={() => tabClick("queue")} name="queue">Queue</p>
+          <p className={navClasses[2]} onClick={() => tabClick("search")} name="search">Search</p>
+        </div>
       </header>
+      <section className="App-body">
+        <div className={"Login Tab"+hiddenTabs[0]}>
+          <div className="Username Input-field">
+            <p className="Label">Username/Email</p>
+            <input type="text" className="Textfield" id="username"/>
+          </div>
+          <div className="Password Input-field">
+            <p className="Label">Password</p>
+            <input type="text" className="Textfield" id="password"/>
+          </div>
+          <div className="Submit Input-field">
+            <input type="submit" className="Login-submit Button" id="login"/>
+          </div>
+        </div>
+        <div className={"Queue Tab"+hiddenTabs[1]}>
+        </div>
+        <div className={"Search Tab"+hiddenTabs[2]}>
+        </div>
+      </section>
     </div>
   );
 }
